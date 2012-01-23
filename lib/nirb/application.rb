e6c07c797dcc8283c46484d94be12cab0351d270
Daniel Nordstrom <d@nintera.com>
2012-01-23 16:04:40 +0100

    Initial commit.

--- lib/nirb/application.rb
+++ lib/nirb/application.rb
@@ -1,13 +1,20 @@
 module Nirb
   class Application
     def initialize
-      @nirb = {}
+      @nirb = {
+        template: 'index.slim'
+      }
     end
 
     def call(env)
       initialize
 
       Rack::Response.new('Hello world').finish
+    end
+
+    def render_template
+      template = @nirb[:template] || '404.slim'
+      Slim::Template.new(template).render(bindings)
     end
   end
 end
